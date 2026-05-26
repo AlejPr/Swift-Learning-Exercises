5be271bcc79790921e27300735994fc8827279db
Alioth <a2provedo@gmail.com>
2026-05-26 09:19:44 -0400

    Initial Commit

---
+++ LearningExercises/LearningExercises.swift
@@ -0,0 +1,9 @@
+//
+//  LearningExercises.swift
+//  LearningExercises
+//
+//  Created by Alejandro on 5/26/26.
+//
+
+import Foundation
+
---
+++ LearningExercisesTests/LearningExercisesTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearningExercisesTests.swift
+//  LearningExercisesTests
+//
+//  Created by Alejandro on 5/26/26.
+//
+
+import Testing
+@testable import LearningExercises
+
+struct LearningExercisesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
